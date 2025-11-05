149a61c5785f9443cb1d73e378643310d9fae576
julieisobelle <git@stackblitz.com>
2025-11-05 10:10:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# glodeH25_undervisning12
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/julieisobelle/glodeH25_undervisning12)
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello!");
+
+const heroTitle = document.querySelector("#heroTitle");
+console.log(heroTitle);
+
+heroTitle.style.color = "Red";
+
+const heroDescription = document.querySelector("#heroDescription");
+console.log(heroDescription);
